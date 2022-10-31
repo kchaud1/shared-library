1aebc76aa11fb133fe1911e2de9a6aad53e00685
krnch <53635223+kchaud1@users.noreply.github.com>
2022-10-31 16:45:29 +0530

    Update runDockerPush.groovy

--- vars/runDockerPush.groovy
+++ vars/runDockerPush.groovy
@@ -225,7 +225,7 @@ def dockerPushEcr(Map config, List serviceNames,String role) {
     
     //['brc/dpe-bloomreachexperience/develop/cms':['akjllsdfjc9i3j2mx','latest']]
     //def filteredImageNameToTagListMap = validateEcr(config, imageNameToTagList)
-    List serviceNamesToPush=generateImagesToPush(Map config, List serviceNames)
+    List serviceNamesToPush=generateImagesToPush(config, serviceNames)
     //push(filteredImageNameToTagListMap, pushRegistryUrl, credSetName)    
     push(config, serviceNamesToPush,role)
 }
