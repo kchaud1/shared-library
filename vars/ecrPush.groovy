768ee26b1f4772219da77a760519f18c5e85a5fc
krnch <53635223+kchaud1@users.noreply.github.com>
2022-11-01 12:56:11 +0530

    Create ecrPush.groovy

---
+++ vars/ecrPush.groovy
@@ -0,0 +1,53 @@
+def call(Map config, List serviceNames=[], String role=null) {
+    if (serviceNames.isEmpty()){serviceNames=utilities.generateServiceNamesListFromConfigMap(config)}
+
+    String dockerPushRegistryLocation= (config["dockerPushRegistryLocation"]==null) ? "hdfc" : config["dockerPushRegistryLocation"] 
+
+    pipelineLogger.info("Entering Docker Push Stage")
+    switch (dockerPushRegistryLocation){
+        case "aws":
+            pipelineLogger.info("Found dockerPushRegistryLocation as 'aws'")
+            dockerPushEcr(config, serviceNames,role)
+            break
+        case "hdfc":
+            pipelineLogger.info("Found dockerPushRegistryLocation as 'hdfc'")
+            dockerPushOnPrem(config, serviceNames)
+            break
+        default:
+            pipelineLogger.error("dockerPushRegistryLocation found in .env as '${dockerPushRegistryLocation}' which does not match any of the known registry types")
+            break
+    }    
+}
+def dockerPushEcr(Map config, List serviceNames,String role) {
+    utilities.checkAwsVars(config)
+    String pushRegistryUrl
+    String credSetName
+    //(pushRegistryUrl, credSetName) = getRegistryProperties(config)
+    //def imageNameToTagList = generateImageNameToTagList(config, serviceNames)
+    
+    //['brc/dpe-bloomreachexperience/develop/cms':['akjllsdfjc9i3j2mx','latest']]
+    //def filteredImageNameToTagListMap = validateEcr(config, imageNameToTagList)
+    List serviceNamesToPush=validateEcr(config, serviceNames,role)
+    List imagesToPush=[]
+  
+    def awsAccountNumber=config["awsAccountNumber"]
+    def awsRegion=config["awsRegion"]
+    def awsUser=config["awsUser"]
+    String dockerPushRegistryLocation=(config["dockerPushRegistryLocation"]==null) ? "ampf" : config["dockerPushRegistryLocation"]  
+
+   
+    def pushRegistryUrl = "${awsAccountNumber}.dkr.ecr.${awsRegion}.amazonaws.com"
+
+        sh(script:""" aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${awsAccountNumber}.dkr.ecr.${awsRegion}.amazonaws.com""", returnStdout: true)
+        String imageName=utilities.generateDockerImageName(serviceName, config)
+        String imageNameWithTag="${imageName}:${env.tag}"
+        
+        pipelineLogger.debug("Pushing image '${pushRegistryUrl}/${imageNameWithTag}'.")
+        docker.image(imageNameWithTag).push()
+        pipelineLogger.debug("Pushing image '${pushRegistryUrl}/${imageName}:latest'.")
+        docker.image(imageNameWithTag).push("latest")
+
+    //push(filteredImageNameToTagListMap, pushRegistryUrl, credSetName)    
+    push(config, serviceNamesToPush,role)
+}
+
