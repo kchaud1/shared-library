084a7eb05319e4d022b34cc7c5037307de507963
krnch <53635223+kchaud1@users.noreply.github.com>
2022-11-02 11:06:07 +0530

    Update utilCalculateTag.groovy

--- vars/utilCalculateTag.groovy
+++ vars/utilCalculateTag.groovy
@@ -1,8 +1,4 @@
-   /**
-    * Wrapper function to get the value of 'release' variable from .env 
-    * @param config - map of properties read in from .env
-    * @return release - string containing the specified value   
-    */
+
 def getRelease(config){
     def final RELEASE_STRING="release"
     def release = ""
@@ -17,28 +13,7 @@ def getRelease(config){
     return release
 }
 
-   /**
-    * Calculate the tag using automatic semantic versioning strategy.  Tag will depend on the branch-
-    *
-    *   If build is happening against the branch that is released to production (could be either prod or release depending on branching strategy):
-    *   The tag will evaluate to something like '1.2.0', where
-    *       -release=1.2 in the .env, and 
-            -There have been zero commits on this branch since it was incremented to 1.2 (that is, THIS commit incremented the version)
-    *   If the next commit leaves the same 'release=1.2', it is assumed this is a bugfix, and will increment the tag to 1.2.1
-    *   To track of this, Jenkins will add a tag into Bitbucket each time it detects that release is incremented.  When you increment
-    *   release=1.3, the tag "1.3" will be applied to that commit automatically
-    *
-    *   If the build is happening on a branch that does not deploy to production (i.e. develop):
-    *   The tag will evaluate to '1.2.0-develop.0', where:
-    *       -release=1.2 in .env file
-    *       -current branch is develop
-    *        -There have been zero commits on this branch since it was incremented to 1.2 (that is, THIS commit incremented the version)
-    *    On the next commit, assuming the release is not incremented again, the tag would be set to '1.2.0-develop.1'
-    *    This indicates that the current work is being done towards a release tagged "1.2.0" which would be build from the production branch
-    *
-    *   @param config - map of properties read in from .env
-    *   @return imageTag - string in the form '1.2.3' or 1.2.0-develop.2   
-    */
+   
 def calculateSemVerTag(config) {
     def final RELEASE_BRANCH_STRING="releaseBranch"
     def defaultReleaseBranch = "release"
@@ -54,23 +29,9 @@ def calculateSemVerTag(config) {
         tag ="${tag}.0-${env.gitBranchName}"
     }
 
-    utilGit.checkCreateGitTag(tag,"",false,config)
-    def optional_tag = utilGit.getOptionalTagging(config)
+    //need to write code to fetch git tag from the repository & if it fails then we have to break the pipeline
+	
     
-    // Adding try/catch condition to check if value of count is found i.e if git tag is found in repo it will return the imagetag. If not it will check if gitTagging=false then it will set count as 0 and return imageTag.
-  	try {
-      if ( optional_tag == "false" ) {
-        //pipelineLogger.info("Git tag validated as false in calculate")
-        def count = sh (script:"git rev-list ${env.gitCommitHash} --count", returnStdout: true).replaceAll("\\s","")
-        def imageTag = "${tag}.${count}"
-        return imageTag
-	  	}
-      else
-      {
-        pipelineLogger.info("tag - ${tag} env.gitCommitHash -  ${env.gitCommitHash} ")
-        def count = sh (script:"git rev-list '${tag}'..${env.gitCommitHash} --count", returnStdout: true).replaceAll("\\s","")
-        def imageTag = "${tag}.${count}"
-        //pipelineLogger.info("${imageTag}")
         return imageTag
       }
     }
@@ -97,20 +58,6 @@ def calculateBuildNumberTag(Map config) {
 }
 
 
-   /**
-    * Calculate the tag for the Docker image based on the tagging strategy
-    * Depending on tagging strategy, may also push tag to Bitbucket
-    *
-    *   Current (8/13/19) tagging strategies include:
-    *    -semVer (see calculateSemVerTag() )
-    *    -buildNumber (see calculateBuildNumberTag)
-    *    -commitHash (commit hash of checkout that is currently building)
-    *    -latestOnly (will only push 'latest' tag, no hard tag - this will be overwritten each time and should only be used for dev environments)
-    *    
-    *    By default each tag will be pushed along with the "latest" tag, with the exception of 'latestOnly'.
-    * @param config - map of properties read in from .env
-    *    
-    */
 def call(Map config) {
     def final PIPELINE_TYPE_STRING="pipelineType"
     def final TAGGING_STRATEGY_STRING="taggingStrategy"
