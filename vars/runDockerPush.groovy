6eb02b115d4f797e1337da9f28a133024ff7f8e4
krnch <53635223+kchaud1@users.noreply.github.com>
2022-11-02 14:48:39 +0530

    Update runDockerPush.groovy

--- vars/runDockerPush.groovy
+++ vars/runDockerPush.groovy
@@ -30,13 +30,10 @@ def dockerPushEcr(Map config, List serviceNames,String role) {
     def awsUser=config["awsUser"]
     String dockerPushRegistryLocation=(config["dockerPushRegistryLocation"]==null) ? "hdfc" : config["dockerPushRegistryLocation"]  
    
-   //def serviceName = config['serviceName']
-    //pushRegistryUrl = "${awsAccountNumber}.dkr.ecr.${awsRegion}.amazonaws.com"
-    pushRegistryUrl = getDockerPushUrlEcr(config)
-        //sh(script: "aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 898815416447.dkr.ecr.ap-south-1.amazonaws.com")
-        sh(script:""" aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${awsAccountNumber}.dkr.ecr.${awsRegion}.amazonaws.com""", returnStdout: true)
-         docker.withRegistry("https://${pushRegistryUrl}") {
-                    pushImages(config, serviceNames, pushRegistryUrl)
+      pushRegistryUrl = getDockerPushUrlEcr(config)
+      sh(script:""" aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${awsAccountNumber}.dkr.ecr.${awsRegion}.amazonaws.com""", returnStdout: true)
+      docker.withRegistry("https://${pushRegistryUrl}") {
+      pushImages(config, serviceNames, pushRegistryUrl)
 
     }
 }
