1f0e607895adc1d92a00d55c8f6cef6bf7b01000
krnch <53635223+kchaud1@users.noreply.github.com>
2022-11-04 00:13:43 +0530

    Update runDockerPush.groovy

--- vars/runDockerPush.groovy
+++ vars/runDockerPush.groovy
@@ -26,7 +26,7 @@ def dockerPushEcr(Map config, List serviceNames,String role) {
     String credSetName
     
     List imagesToPush=[]
-    String imageName=utilities.generateDockerImageName(serviceNames, config)
+    //String imageName=utilities.generateDockerImageName(serviceNames, config)
     def awsAccountNumber=config["awsAccountNumber"]
     def awsRegion=config["awsRegion"]
     def awsUser=config["awsUser"]
